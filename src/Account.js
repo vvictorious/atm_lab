7e1f33074d72bdd90a1b5e2e62709bc259270ba1
vvictorious <vvictorious30@gmail.com>
2018-05-03 14:02:33 -0600

    got through step 5

--- src/Account.js
+++ src/Account.js
@@ -7,13 +7,23 @@ export default class Account extends Component {
             balance: 0
           }
         }
+        handleDepositClick(e) {
+          e.preventDefault();
+          let amount = parseInt(this.inputBox.value);
+          let newBalance = this.state.balance + amount;
+          this.setState({
+            balance: newBalance
+             })
+            this.inputBox.value = '';
+          }
   render() {
     return (
       <div className="account">
+        
         <h2> { this.props.name } </h2>
         <div className="balance">$0</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
-        <input type="button" value="Deposit" />
+        <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
         <input type="button" value="Withdraw" />
       </div>
     )
