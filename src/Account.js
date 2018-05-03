a34c4fe1ed63e0d334eabd77fd8da891e470a899
vvictorious <vvictorious30@gmail.com>
2018-05-03 08:29:15 -0600

    finished step 2

--- src/Account.js
+++ src/Account.js
@@ -4,12 +4,12 @@ export default class Account extends Component {
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2> { this.props.name } </h2>
         <div className="balance">$0</div>
         <input type="text" placeholder="enter an amount" />
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
     )
-  }
+  }ß
 }
